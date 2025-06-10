cc3202dd47c0a5a1c5557191ac6277e388b7a5d0
Prathamesh Jakkula <prathamesh.jakkula.01042005@gmail.com>
2025-06-10 11:46:55 +0530

    input added

--- app/student/page.jsx
+++ app/student/page.jsx
@@ -2,21 +2,38 @@
 
 import Header from "@/component/header";
 import { Button } from "@/components/ui/button";
-import { useState } from 'react'
+import { useState } from 'react';
 
-export default function Student(){
-    const [count,setCount]=useState("this is a string");
-    function submit(){
-        setCount("i changed the string");
-        console.log("button pressed");
-    }
-    return(
-        <>
-        <Header/>
-         {count}
-        <h1 style={{margin:"5px"}}>Student1</h1>
-        <h1 className="m-5">Student2</h1>
-        <Button onClick={submit}>click me</Button>
-        </>
-    )
+export default function Student() {
+  const [count, setCount] = useState("this is a string");
+  const [user, setUser] = useState("");
+
+  function submit() {
+    setCount("i changed the string");
+    console.log("button pressed");
+  }
+
+  function handleChange(e) {
+    setUser(e.target.value);
+  }
+
+  return (
+    <>
+      <Header />
+      <p>{count}</p>
+{user}
+      <h1 style={{ margin: "5px" }}>Student1</h1>
+      <h1 className="m-5">Student2</h1>
+
+      <Button onClick={submit}>click me</Button>
+
+      <input
+        type="text"
+        value={user}
+        onChange={handleChange}
+        className="border p-2 rounded"
+        placeholder="Enter your name"
+      />
+    </>
+  );
 }
